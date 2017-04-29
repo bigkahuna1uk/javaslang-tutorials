b1a45bbf56eeba4924028a80cf195c8cdbf6e2ea
Chris Kelly <bigkahuna1uk-git@gmail.com>
2017-04-29 23:57:23 +0100

    Added JUnit 5 deps so IntelliJ can run IDE tests

--- src/test/java/com/usul/training/javaslang/CurryingTest.java
+++ src/test/java/com/usul/training/javaslang/CurryingTest.java
@@ -2,6 +2,7 @@ package com.usul.training.javaslang;
 
 import javaslang.Function1;
 import javaslang.Function2;
+import javaslang.Function4;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,18 +18,27 @@ public class CurryingTest {
 
     private Function2<Integer, Integer, Integer> sum;
 
+    private Function4<Integer, Integer, Integer, Integer, Integer> sum4;
+
+
     @BeforeEach
-    public void setup()
-    {
+    public void setup() {
         sum = (a, b) -> a + b;
+        sum4 = (a, b, c, d) -> a + b + c + d;
     }
 
     @Test
-    public void currying()
-    {
+    public void currying() {
         Function1<Integer, Integer> curried = sum.curried().apply(2);
 
         then(curried.apply(4)).isEqualTo(6);
+    }
+
+    @Test
+    public void curryingHald() {
+        Function2<Integer, Integer, Integer> curried2 = sum4.apply(2, 2);
+
+        then(curried2.apply(3,3)).isEqualTo(10);
     }
 
 
